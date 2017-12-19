df533bba85b0dc5ec6b5285665bdff573612d00a
Sophie Kolbe <sophie.kolbe@zalando.de>
2017-12-19 14:47:42 +0100

    load data from good data project into json format locally

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//fetch component from gooddata SDK
 import { Kpi } from '@gooddata/react-components';
 import '@gooddata/react-components/styles/css/main.css';
+
+//loading data from your project on gooddata with all attributes and store them in a json file by using
+// > gdc-catalog-export --project-id la84vcyhrq8jwbu4wpipw66q2sqeb923 --username <your-gooddata-username> --password <your-password> --output src/catalog.json
+import { CatalogHelper } from '@gooddata/react-components';
+import catalogJson from './catalog.json';
+const C = new CatalogHelper(catalogJson);
 
 class App extends Component {
   render() {
